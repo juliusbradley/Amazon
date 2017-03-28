2c2045e27beedd37a171e92989051e0a8ef393c1
Julius Bradley <julius.b.bradley@gmail.com>
2017-03-28 14:54:47 -0700

    Initial Commit:Empty Rails App with Welcome, About and Contact Controllers

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,16 @@
+class ContactController < ApplicationController
+  def index
+  end
+
+  def create
+  #if you want to define a variable that you can access in the view file then
+  # you will need to define the variable as an instance variable as in:
+  @name = params[:name] #you can access inside view file
+                       #such as : create.html.erb
+
+  #this will render a plain text response that has a body of
+  #'Thank you for contacting us'
+#render plain: "Thank you #{params[:name]} for contacting us"
+redirect_to contact_path({name: params[:name]})
+end
+end
